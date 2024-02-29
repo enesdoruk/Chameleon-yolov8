bd89d633b5172bd7e992e8b4f3917a06a74b6f09
enesdoruk <enes.doruk@adastec.com>
2024-02-29 15:07:36 +0300

    feat: multi scale with dilation

--- syndet/multi_scale_alg.py
+++ syndet/multi_scale_alg.py
@@ -6,22 +6,20 @@ class MultiScaleAlig(nn.Module):
     def __init__(self) -> None:
         super(MultiScaleAlig, self).__init__()
         
-        self.br2_1 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2)
+        self.br2_1 = nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=2, dilation=2)
         self.prelu2_1 = nn.PReLU()
-        self.mpool2_1 = nn.MaxPool2d(kernel_size=3, stride=2)
         
-        self.br3_1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.br3_1 = nn.Conv2d(256, 256, kernel_size=3, stride=4, padding=2, dilation=2)
         self.prelu3_1 = nn.PReLU()
-        self.mpool3_1 = nn.MaxPool2d(kernel_size=3, stride=4)
         
         self.conv = nn.Conv2d(1024+512+256, 1024+512+256, kernel_size=1)
         self.prelu = nn.PReLU()
             
     def forward(self, in_br1, in_br2, in_br3) -> torch.Tensor: 
         import pdb; pdb.set_trace()
-        out_br2_1 = self.mpool2_1(self.prelu2_1(self.br2_1(in_br2)))
+        out_br2_1 = self.prelu2_1(self.br2_1(in_br2))
         
-        out_br3_1 = self.mpool3_1(self.prelu3_1(self.br3_1(in_br3)))
+        out_br3_1 = self.prelu3_1(self.br3_1(in_br3))
         
         out_cat = torch.cat([in_br1, out_br2_1, out_br3_1], dim=1)
         
