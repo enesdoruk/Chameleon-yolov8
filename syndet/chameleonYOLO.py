88268ae27ad48a72ff52710c0123a5ff8be132f3
enesdoruk <enes.doruk@adastec.com>
2024-03-07 19:42:51 +0300

    feat: channel attention

--- syndet/chameleonYOLO.py
+++ syndet/chameleonYOLO.py
@@ -1,12 +1,6 @@
-import cv2
 import torch
-import wandb
-import numpy as np
 import torch.nn as nn
-from scipy.ndimage import zoom
-from mmengine.visualization import Visualizer
 
-import os
 import sys
 sys.path.insert(0, "/AI/syndet-yolo")
 
@@ -15,13 +9,16 @@ from syndet.backbone import Backbone
 from syndet.head import Head
 from syndet.discriminator import Discriminator
 from syndet.multi_scale_alg import MultiScaleAlig
+from syndet.channel_attention import ChannelAttention
 
 
 class DetectionModel(nn.Module):
     def __init__(self):
         super(DetectionModel, self).__init__()
         
+        
         self.layers = []
+        self.layers.append(ChannelAttention(in_planes=1024+512+256, ratio=4))
         self.layers.append(MultiScaleAlig())
         self.layers.append(Discriminator(num_convs=4, in_channels=1792, grad_reverse_lambda=0.02))
         self.layers.append(Backbone())
@@ -38,30 +35,36 @@ class DetectionModel(nn.Module):
             m.bias_init() 
 
     def forward(self, source, target=None):      
-        backb5, backb7, backb10 = self.model[2](source)
+        backb5, backb7, backb10 = self.model[3](source)
         
         if target is None:
-            head, _ = self.model[3](backb5,
+            head, _ = self.model[4](backb5,
                                 backb7, 
                                 backb10)  
             return head
         
         else:
-            backb5_t, backb7_t, backb10_t = self.model[2](target)    
+            backb5_t, backb7_t, backb10_t = self.model[3](target)    
             
-            head, head_convs_s = self.model[3](backb5,
+            head, head_convs_s = self.model[4](backb5,
                                 backb7, 
                                 backb10)  
             
-            _, head_convs_t = self.model[3](backb5_t,
+            _, head_convs_t = self.model[4](backb5_t,
                                 backb7_t, 
                                 backb10_t)  
             
-            source_disc_feat = self.model[0](head_convs_t[0], head_convs_t[1], head_convs_t[2])
-            target_disc_feat = self.model[0](head_convs_s[0], head_convs_s[1], head_convs_s[2])
+            source_disc_feat = self.model[1](head_convs_t[0], head_convs_t[1], head_convs_t[2])
+            target_disc_feat = self.model[1](head_convs_s[0], head_convs_s[1], head_convs_s[2])
             
-            grl_b10_s = self.model[1](source_disc_feat, 0, grl=True)            
-            grl_b10_t = self.model[1](target_disc_feat, 1, grl=True)
+            source_att = self.model[0](source_disc_feat)
+            target_att = self.model[0](target_disc_feat)
+            
+            source_feat_att = torch.mul(source_disc_feat, source_att)
+            target_feat_att = torch.mul(target_disc_feat, target_att)
+
+            grl_b10_s = self.model[2](source_feat_att, 0, grl=True)            
+            grl_b10_t = self.model[2](target_feat_att, 1, grl=True)
             
             adv_loss = grl_b10_s + grl_b10_t
             
